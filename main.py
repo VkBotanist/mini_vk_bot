f47368b7ab4c436d33cc1b24baf9b89597235a80
Ilya Petrash <ilya.petrash@inbox.ru>
2017-08-06 18:55:35 +0500

    Бот теперь обрабатывает команды пользователя, от которого запущен

--- main.py
+++ main.py
@@ -45,15 +45,32 @@ import sys
 sys.excepthook = log_uncaught_exceptions
 
 
+import time
+
+
 if __name__ == '__main__':
     import vk_api
     vk = vk_api.VkApi(login=LOGIN, password=PASSWORD)
     vk.auth()
     log.debug("Бот запущен")
 
-    command_prefix = 'Бот,'
+    # BOT_USER_ID = vk.method('users.get')[0]['id']
+    # time.sleep(0.3)
 
-    last_message_bot_id = None
+    # NOTE: для запроса сообщений
+    # messages_get_values = {
+    #     'out': 0,
+    #     'count': 100,
+    #     'version': '5.67'
+    # }
+    # rs = vk.method('messages.get', messages_get_values)
+    # print(rs)
+    # for i in sorted(rs['items'], key=lambda x: x['date']):
+    #     print(i)
+    #
+    # quit()
+
+    command_prefix = 'Бот,'
 
     messages_get_values = {
         'out': 0,
@@ -62,75 +79,98 @@ if __name__ == '__main__':
         'version': '5.67'
     }
 
+    messages_get_values__out = {
+        'out': 1,
+        'count': 1,
+        'time_offset': 60,
+        'version': '5.67'
+    }
+
+    def messages_get(out=0):
+        if not out:
+            rs = vk.method('messages.get', messages_get_values)
+        else:
+            rs = vk.method('messages.get', messages_get_values__out)
+        # log.debug(rs)
+
+        # Если ничего не пришло
+        if not rs['items']:
+            return
+
+        item = rs['items'][0]
+        message_id = item['id']
+        from_user_id = item['user_id']
+        message = item['body']
+
+        # Если сообщение пришло из групповой беседы, в chat_id будет число, иначе None
+        chat_id = item.get('chat_id')
+
+        # Бот реагирует только на сообщения, начинающиеся с префикса
+        if not message.lower().startswith(command_prefix.lower()):
+            return
+
+        if chat_id:
+            log.debug('From chat #%s from user #%s, message (#%s): "%s"',
+                      chat_id, from_user_id, message_id, message)
+        else:
+            log.debug('From user #%s, message (#%s): "%s"', from_user_id, message_id, message)
+
+        command = message[len(command_prefix):].strip()
+
+        # TODO: для каждой команды отдельный поток создавать
+        # Пример: https://www.ibm.com/developerworks/ru/library/l-python_part_9/index.html#N10078
+        # только поток нужно без демона использовать, демон указывает что при закрытии главного потока
+        # должны и потоки-демоны завершиться, что для текущей задачи не будет правильным
+
+        # Выполнение команды
+        try:
+            import commands
+            message = commands.execute(command)
+
+        except Exception as e:
+            log.exception("Error:")
+
+            import traceback
+            message = 'При выполнении команды "{}" произошла ошибка: ' \
+                      '"{}":\n\n{}'.format(command, e, traceback.format_exc())
+
+        # Если ответа от бота нет
+        if not message:
+            message = 'Не получилось выполнить команду "{}" :( Попробуй позже повторить :)'.format(command)
+
+        log.debug('Message: "%s"', message)
+
+        messages_send_values = {
+            'message': message,
+            'version': '5.67'
+        }
+
+        # Если сообщение пришло из групповой беседы
+        if chat_id:
+            messages_send_values['chat_id'] = chat_id
+        else:
+            messages_send_values['user_id'] = from_user_id
+
+        time.sleep(0.3)
+        last_message_bot_id = vk.method('messages.send', messages_send_values)
+
+        if not out:
+            messages_get_values['last_message_id'] = last_message_bot_id
+        else:
+            messages_get_values__out['last_message_id'] = last_message_bot_id
+
     while True:
         try:
-            rs = vk.method('messages.get', messages_get_values)
-            # log.debug(rs)
+            messages_get()
 
-            # Если ничего не пришло
-            if not rs['items']:
-                continue
+            # TODO: временно, сделать красиво
+            time.sleep(1)
 
-            item = rs['items'][0]
-            message_id = item['id']
-            from_user_id = item['user_id']
-            message = item['body']
+            messages_get(out=1)
 
-            # Если сообщение пришло из групповой беседы, в chat_id будет число, иначе None
-            chat_id = item.get('chat_id')
-
-            # Бот реагирует только на сообщения, начинающиеся с префикса
-            if not message.lower().startswith(command_prefix.lower()):
-                continue
-
-            if chat_id:
-                log.debug('From chat #%s from user #%s, message (#%s): "%s"',
-                          chat_id, from_user_id, message_id, message)
-            else:
-                log.debug('From user #%s, message (#%s): "%s"', from_user_id, message_id, message)
-
-            command = message[len(command_prefix):].strip()
-
-            # TODO: для каждой команды отдельный поток создавать
-            # Пример: https://www.ibm.com/developerworks/ru/library/l-python_part_9/index.html#N10078
-            # только поток нужно без демона использовать, демон указывает что при закрытии главного потока
-            # должны и потоки-демоны завершиться, что для текущей задачи не будет правильным
-            
-            # Выполнение команды
-            try:
-                import commands
-                message = commands.execute(command)
-
-            except Exception as e:
-                log.exception("Error:")
-
-                import traceback
-                message = 'При выполнении команды "{}" произошла ошибка: ' \
-                          '"{}":\n\n{}'.format(command, e, traceback.format_exc())
-
-            # Если ответа от бота нет
-            if not message:
-                message = 'Не получилось выполнить команду "{}" :( Попробуй позже повторить :)'.format(command)
-
-            log.debug('Message: "%s"', message)
-
-            messages_send_values = {
-                'message': message,
-                'version': '5.67'
-            }
-
-            # Если сообщение пришло из групповой беседы
-            if chat_id:
-                messages_send_values['chat_id'] = chat_id
-            else:
-                messages_send_values['user_id'] = from_user_id
-
-            last_message_bot_id = vk.method('messages.send', messages_send_values)
-            messages_get_values['last_message_id'] = last_message_bot_id
 
         except Exception as e:
             log.exception('Error:')
 
         finally:
-            import time
-            time.sleep(3)
+            time.sleep(1)
